1484ae1a9eee30d957327711b588e7da505b80ee
Cristian Fernández <129860008+Cristian-000@users.noreply.github.com>
2024-07-21 10:34:21 -0300

    Update usuario.js

--- usuario.js
+++ usuario.js
@@ -1,1 +1,6 @@
-
+// Event listener para el botón de regresar en escribir.html
+const backButtonUsuario = document.getElementById('back-button-usuario');
+if (backButtonUsuario) {
+    backButtonUsuario.addEventListener('click', () => {
+        window.history.back();
+    });
