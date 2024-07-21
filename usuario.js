d26ee84e4f131484a98d083f131231aa9a07468c
Cristian Fernández <129860008+Cristian-000@users.noreply.github.com>
2024-07-21 19:24:14 -0300

    Update usuario.js

--- usuario.js
+++ usuario.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Mostrar mensaje si no hay datos guardados
     if (!usuario.nombre && !usuario.apellido && !usuario.institucion && !usuario.direccion) {
-        fichaContainer.innerHTML = "<p>Aún no se han añadido datos de usuario</p>";
+        fichaContainer.innerHTML ="<p>Aún no se han añadido datos de usuario</p>" + formToggle;
     } else {
         // Cargar datos del usuario
         if (usuario.nombre) {
